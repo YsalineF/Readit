cbeb1e599e298ca21fce79a686dca1ffe887af44
Mézz <ysaline.franquet@gmail.com>
2020-05-18 14:54:16 +0200

    Structure MVC initiale
    Mise en place de la structure MVC initiale

---
+++ app/vues/template/index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  ../app/vues/template/index.php
+  TEMPLATE GENERAL
+*/
+ ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Titre</title>
+   </head>
+   <body>
+     <h2>Coucou</h2>
+   </body>
+ </html>
